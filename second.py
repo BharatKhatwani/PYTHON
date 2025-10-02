315bc076cba93e97a2b9fe539006dd74f73c08a8
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-10-02 22:33:34 +0000

    'inner details'

---
+++ second.py
@@ -0,0 +1,8 @@
+# Byte code is not a machine code
+# there __pycache__ this folder is only be created when you work with  the imported 
+# python specific interpetion
+
+
+
+from main import chai 
+chai("bharat")
