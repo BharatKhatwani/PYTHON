848260ba9858b872b8d6935fdae5a097862f8976
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-10-06 02:01:41 +0000

    'added question'

---
+++ 03loop/03question.py
@@ -0,0 +1,6 @@
+n = 3
+
+for i in range(1, 10):
+    if i == 5:
+        continue
+    print(n, 'x', i, '=', n * i)
---
+++ 03loop/07question.py
@@ -0,0 +1,9 @@
+
+
+while True :
+    number =  input("Enter value btw 1 and 10")
+    if(1 <= number <= 10 ):
+        print("Thanks")
+        break
+    else :
+        print("Invalid number try again ")
